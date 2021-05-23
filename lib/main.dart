21a27954d826c0fc37a5b31532de477f5ea14039
Andres Noboa <andresnoboa17@hotmail.com>
2021-05-23 01:45:56 -0500

    Refactoring the widget to a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
   void answerQuestion() {
+    setState(() => {questionIndex = questionIndex + 1});
     print("Answer chosen!");
   }
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?",
+    ];
+    var answers = [
+      "Red",
+      "Blue",
+      "Green",
+    ];
+
     return MaterialApp(
       title: "My First App",
       home: Scaffold(
@@ -18,17 +38,17 @@ class MyApp extends StatelessWidget {
         body: Center(
           child: Column(
             children: <Widget>[
-              Text("What is your name?"),
+              Text(questions[questionIndex]),
               RaisedButton(
-                child: Text("Answer 1"),
+                child: Text(answers[0]),
                 onPressed: answerQuestion,
               ),
               RaisedButton(
-                child: Text("Answer 2"),
+                child: Text(answers[1]),
                 onPressed: () => print("Pressed with anonymous function!"),
               ),
               RaisedButton(
-                child: Text("Answer 3"),
+                child: Text(answers[2]),
                 onPressed: answerQuestion,
               ),
             ],
